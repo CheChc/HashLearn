0adc4a2bdd6432997faac59d61849a7683f11ad5
CheChc <3331394002@qq.com>
2024-02-03 00:36:32 +0800

    line

--- IsAnagram.java
+++ IsAnagram.java
@@ -2,25 +2,26 @@ public class IsAnagram
 {
     public boolean isAnagram(String s, String t)
     {
-        int[] record=new int[26];
-        for(int i=0;i<s.length();i++)
+        int[] record = new int[26];
+        for (int i = 0; i < s.length(); i++)
         {
-            record[s.charAt(i)-'a']++;
+            record[s.charAt(i) - 'a']++;
         }
-        for(int j=0;j<t.length();j++)
+        for (int j = 0; j < t.length(); j++)
         {
-            record[t.charAt(j)-'a']--;
+            record[t.charAt(j) - 'a']--;
         }
-        for(int k=0;k<26;k++)
+        for (int k = 0; k < 26; k++)
         {
-            if(record[k]!=0)
+            if (record[k] != 0)
                 return false;
         }
         return true;
 
     }
+
     public static void main(String[] args)
     {
-        
+
     }
 }
